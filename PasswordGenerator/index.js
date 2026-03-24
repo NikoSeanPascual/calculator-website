37fbc45d2a069bd482fb2f5e4d1498765f7d381c
NikoSeanPascual <nikoseanpascual@gmail.com>
2026-03-24 16:38:29 +0800

    Optimize Codes to make it more readable and added Password length and a slider

--- PasswordGenerator/index.js
+++ PasswordGenerator/index.js
@@ -1,59 +1,44 @@
 const characters = [
-  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "/", "?", "`", "~"
+];
 
-  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+const lengthEl = document.getElementById("length-el");
+const lengthVal = document.getElementById("length-val");
+const passwordOneText = document.querySelector("#password-one-el .password-text");
+const passwordTwoText = document.querySelector("#password-two-el .password-text");
 
-  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-
-  "!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
-  "_", "+", "-", "=", "[", "]", "{", "}", "|",
-  ";", ":", "'", ",", ".", "<", ">", "/", "?", "`", "~"
-]
-let passwordOneEl = document.getElementById("password-one-el")
-let passwordTwoEl = document.getElementById("password-two-el")
-
-let passwordOneText = passwordOneEl.querySelector(".password-text")
-let passwordTwoText = passwordTwoEl.querySelector(".password-text")
-
-passwordOneEl.addEventListener("click", function() {
-    let tooltip = passwordOneEl.querySelector(".tooltip")
-
-    navigator.clipboard.writeText(passwordOneText.textContent)
-        .then(() => {
-            tooltip.textContent = "Copied!"
-
-            setTimeout(function() {
-                tooltip.textContent = "Copy Password"
-            }, 1000)
-        })
-})
-
-passwordTwoEl.addEventListener("click", function() {
-    let tooltip = passwordTwoEl.querySelector(".tooltip")
-
-    navigator.clipboard.writeText(passwordTwoText.textContent)
-        .then(() => {
-            tooltip.textContent = "Copied!"
-
-            setTimeout(function() {
-                tooltip.textContent = "Copy Password"
-            }, 1000)
-        })
-})
+// Update number display when sliding
+lengthEl.addEventListener("input", function() {
+    lengthVal.textContent = lengthEl.value;
+});
 
 function generatePassword() {
-    let password1 = ""
-    let password2 = ""
+    let pass1 = "";
+    let pass2 = "";
+    const length = lengthEl.value;
 
-    for (let i = 0; i < 15; i++) {
-        let randomIndex1 = Math.floor(Math.random() * characters.length)
-        let randomIndex2 = Math.floor(Math.random() * characters.length)
-        password1 += characters[randomIndex1]
-        password2 += characters[randomIndex2]
+    for (let i = 0; i < length; i++) {
+        pass1 += characters[Math.floor(Math.random() * characters.length)];
+        pass2 += characters[Math.floor(Math.random() * characters.length)];
     }
 
-    passwordOneText.textContent = password1
-    passwordTwoText.textContent = password2
+    passwordOneText.textContent = pass1;
+    passwordTwoText.textContent = pass2;
 }
+
+// Copy functionality
+function setupCopy(el, textEl) {
+    el.addEventListener("click", function() {
+        const tooltip = el.querySelector(".tooltip");
+        navigator.clipboard.writeText(textEl.textContent).then(() => {
+            tooltip.textContent = "Copied!";
+            setTimeout(() => { tooltip.textContent = "Copy Password"; }, 1000);
+        });
+    });
+}
+
+setupCopy(document.getElementById("password-one-el"), passwordOneText);
+setupCopy(document.getElementById("password-two-el"), passwordTwoText);
